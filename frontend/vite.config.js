38dd967d2e3251031010ba250e47796cf07fffc9
IndujaThathsarani <it23818316@my.sliit.lk>
2026-04-17 18:21:57 +0530

    resource module errors fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(), tailwindcss()],
-=======
   plugins: [react()],
-   server: {
+  server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8081',
@@ -16,5 +12,4 @@ export default defineConfig({
       }
     }
   }
->>>>>>> db5afb5cb4cae49701c21e9ee226a0efe0feae33
 })
